c0efa91203bbfb9b597651e88d5e005466283af2
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-04-12 16:58:30 +0700

    add config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,18 +1,39 @@
 package config
 
 import (
-	"github.com/joho/godotenv"
+	"sync"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
 )
 
-type HTTPConfig interface {
-	Address() string
+type Config struct {
+	HTTPAddr       string        `env:"HTTP_ADDR" env-default:":8080"`
+	ReadTimeout    time.Duration `env:"READ_TIMEOUT" env-default:"5s"`
+	WriteTimeout   time.Duration `env:"WRITE_TIMEOUT" env-default:"10s"`
+	IdleTimeout    time.Duration `env:"IDLE_TIMEOUT" env-default:"60s"`
+	MaxHeaderBytes int           `env:"MAX_HEADER_BYTES" env-default:"1048576"`
+
+	ProxyHTTPAddr       string        `env:"PROXY_HTTP_ADDR" env-default:":8081"`
+	ProxyReadTimeout    time.Duration `env:"PROXY_READ_TIMEOUT" env-default:"30s"`
+	ProxyWriteTimeout   time.Duration `env:"PROXY_WRITE_TIMEOUT" env-default:"30s"`
+	ProxyIdleTimeout    time.Duration `env:"PROXY_IDLE_TIMEOUT" env-default:"60s"`
+	ProxyMaxHeaderBytes int           `env:"PROXY_MAX_HEADER_BYTES" env-default:"1048576"`
 }
 
-func Load(path string) error {
-	err := godotenv.Load(path)
-	if err != nil {
-		return err
-	}
+var (
+	instance *Config
+	once     sync.Once
+)
 
-	return nil
+func LoadConfig() *Config {
+	once.Do(func() {
+		cfg := &Config{}
+		err := cleanenv.ReadEnv(cfg)
+		if err != nil {
+			panic(err)
+		}
+		instance = cfg
+	})
+	return instance
 }

3b7d158e4b67a08e8d7b20dbde4e7869bff295d4
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-04-18 18:51:12 +0700

    change port

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,13 +8,13 @@ import (
 )
 
 type Config struct {
-	HTTPAddr       string        `env:"HTTP_ADDR" env-default:":8080"`
+	HTTPAddr       string        `env:"HTTP_ADDR" env-default:":8081"`
 	ReadTimeout    time.Duration `env:"READ_TIMEOUT" env-default:"5s"`
 	WriteTimeout   time.Duration `env:"WRITE_TIMEOUT" env-default:"10s"`
 	IdleTimeout    time.Duration `env:"IDLE_TIMEOUT" env-default:"60s"`
 	MaxHeaderBytes int           `env:"MAX_HEADER_BYTES" env-default:"1048576"`
 
-	ProxyHTTPAddr       string        `env:"PROXY_HTTP_ADDR" env-default:":8081"`
+	ProxyHTTPAddr       string        `env:"PROXY_HTTP_ADDR" env-default:":8082"`
 	ProxyReadTimeout    time.Duration `env:"PROXY_READ_TIMEOUT" env-default:"30s"`
 	ProxyWriteTimeout   time.Duration `env:"PROXY_WRITE_TIMEOUT" env-default:"30s"`
 	ProxyIdleTimeout    time.Duration `env:"PROXY_IDLE_TIMEOUT" env-default:"60s"`

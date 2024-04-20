0e2d9e9dfd8185743095bb26c8a9a2feffa0b508
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-04-20 19:34:11 +0700

    update config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,39 +1,58 @@
 package config
 
 import (
-	"sync"
+	"log"
+	"os"
 	"time"
 
 	"github.com/ilyakaznacheev/cleanenv"
 )
 
+const defaultConfigPath = ".env"
+
 type Config struct {
-	HTTPAddr       string        `env:"HTTP_ADDR" env-default:":8081"`
-	ReadTimeout    time.Duration `env:"READ_TIMEOUT" env-default:"5s"`
-	WriteTimeout   time.Duration `env:"WRITE_TIMEOUT" env-default:"10s"`
+	HTTPAddr       string        `env:"HTTP_ADDR" env-default:":80"`
+	ReadTimeout    time.Duration `env:"READ_TIMEOUT" env-default:"60s"`
+	WriteTimeout   time.Duration `env:"WRITE_TIMEOUT" env-default:"60s"`
 	IdleTimeout    time.Duration `env:"IDLE_TIMEOUT" env-default:"60s"`
 	MaxHeaderBytes int           `env:"MAX_HEADER_BYTES" env-default:"1048576"`
-
-	ProxyHTTPAddr       string        `env:"PROXY_HTTP_ADDR" env-default:":8082"`
-	ProxyReadTimeout    time.Duration `env:"PROXY_READ_TIMEOUT" env-default:"30s"`
-	ProxyWriteTimeout   time.Duration `env:"PROXY_WRITE_TIMEOUT" env-default:"30s"`
-	ProxyIdleTimeout    time.Duration `env:"PROXY_IDLE_TIMEOUT" env-default:"60s"`
-	ProxyMaxHeaderBytes int           `env:"PROXY_MAX_HEADER_BYTES" env-default:"1048576"`
+	AllowedOrigins []string      `env:"ALLOWED_ORIGINS" env-separator:"," env-description:"List of allowed sources for CORS"`
 }
 
-var (
-	instance *Config
-	once     sync.Once
-)
-
+// LoadConfig загружает конфигурацию из переменных окружения или файла конфигурации.
 func LoadConfig() *Config {
-	once.Do(func() {
-		cfg := &Config{}
-		err := cleanenv.ReadEnv(cfg)
-		if err != nil {
-			panic(err)
+	cfg := &Config{}
+
+	// Проверяем значение переменной окружения AMVERA
+	amveraVar, exists := os.LookupEnv("AMVERA")
+
+	if exists && amveraVar == "1" {
+		// Загрузка конфигурации из переменных окружения (облако Amvera)
+		if err := cleanenv.ReadEnv(cfg); err != nil {
+			log.Fatalf("Failed to load configuration from environment variables: %v", err)
 		}
-		instance = cfg
-	})
-	return instance
+	} else {
+		// Загрузка конфигурации из файла конфигурации и переменных окружения
+		configPath := getConfigPath()
+		if err := cleanenv.ReadConfig(configPath, cfg); err != nil {
+			log.Fatalf("Failed to load configuration from file %s: %v", configPath, err)
+		}
+
+		// Загрузка конфигурации из переменных окружения
+		if err := cleanenv.ReadEnv(cfg); err != nil {
+			log.Fatalf("Failed to load configuration from environment variables: %v", err)
+		}
+	}
+
+	return cfg
+}
+
+// getConfigPath возвращает путь к файлу конфигурации из переменной окружения CONFIG_PATH
+// или возвращает путь по умолчанию, если переменная окружения не задана.
+func getConfigPath() string {
+	configPath, ok := os.LookupEnv("CONFIG_PATH")
+	if !ok {
+		configPath = defaultConfigPath
+	}
+	return configPath
 }

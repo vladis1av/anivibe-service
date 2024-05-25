e45cf4eb47b8de8a71008733848a4f5bf3685096
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-05-25 11:06:21 +0700

    add feedback and sitemap routers

--- internal/infrastructure/routers/http/api/routers.go
+++ internal/infrastructure/routers/http/api/routers.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	v1 "anivibe-service/internal/infrastructure/routers/http/api/v1"
+	"anivibe-service/internal/infrastructure/services/feedback"
 	"log"
 
 	"github.com/gorilla/mux"
@@ -9,11 +10,15 @@ import (
 
 const prefixV1 = "/api/v1"
 
-func SetupAPIRouters(mainRouter *mux.Router) {
+func SetupAPIRouters(mainRouter *mux.Router, adminUserID int64) {
 	log.Println("setup routers")
 
 	mainRouterV1 := mainRouter.PathPrefix(prefixV1).Subrouter()
 
+	feedbackService := feedback.NewFeedbackService(adminUserID)
+
 	v1.SetupV1MangaRouters(mainRouterV1)
 	v1.SetupV1ImageRouters(mainRouterV1)
+	v1.SetupV1SitemapRouters(mainRouterV1)
+	v1.SetupV1FeedbackRouters(mainRouterV1, feedbackService)
 }

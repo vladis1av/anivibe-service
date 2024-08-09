1dbfd3a83f2a78aef51bed892efdc39fee77bfcb
vladis1av <vladfrolov.dev@gmail.com>
2024-08-09 22:34:39 +0700

    update headers

--- internal/utils/fetchImage.go
+++ internal/utils/fetchImage.go
@@ -10,7 +10,11 @@ import (
 func FetchImage(url string, referer string, authority string) (io.ReadCloser, error) {
 	client := &http.Client{
 		Timeout: 30 * time.Second,
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return nil // Разрешить все редиректы
+		},
 	}
+
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		return nil, err
@@ -23,7 +27,11 @@ func FetchImage(url string, referer string, authority string) (io.ReadCloser, er
 	if authority == "" {
 		authority = GetAuthorityFromUrl(url)
 	}
+
 	req.Header.Set("Authority", authority)
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+	req.Header.Set("Accept", "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8")
+	req.Header.Set("Accept-Language", "en-US,en;q=0.9")
 
 	resp, err := client.Do(req)
 	if err != nil {
